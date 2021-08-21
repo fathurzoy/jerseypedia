de0df344deb9b56b5436cf49fd7ca469e8275de7
fathurzoy <fathurrahmanf123@gmail.com>
2021-08-21 23:30:50 +0700

    Halaman Home

--- app/Product.php
+++ app/Product.php
@@ -7,4 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     //
+    public function liga()
+    {
+        return $this->belongsTo(Liga::class, 'liga_id', 'id');
+    }
+
+    public function pesanan_details()
+    {
+        return $this->hasMany(PesananDetail::class, 'product_id', 'id');
+    }
 }
